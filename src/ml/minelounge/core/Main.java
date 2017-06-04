c356fddd08baf02f355b6a6776a52e297bac55b0
Casper <casper.dennijs@gmail.com>
2017-06-04 16:50:29 +0200

    Join / Leave messages

--- src/ml/minelounge/core/Main.java
+++ src/ml/minelounge/core/Main.java
@@ -1,7 +1,23 @@
 package ml.minelounge.core;
 
+import org.bukkit.ChatColor;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
-
+public class Main extends JavaPlugin implements Listener {
+	
+	public void onEnable(){
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+	
+	public void JoinMessage(PlayerJoinEvent event){
+		event.setJoinMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GREEN + "+" + ChatColor.DARK_GRAY + "]");
+		
+	}
+	public void QuitMessage(PlayerQuitEvent event){
+		event.setQuitMessage(ChatColor.DARK_GRAY + "[" + ChatColor.RED + "-" + ChatColor.DARK_GRAY + "]");
+		
+	}
 }
