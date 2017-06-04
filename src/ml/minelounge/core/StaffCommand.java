6014702ef50b0dd4cc4908841aa02d88c053e803
Casper <casper.dennijs@gmail.com>
2017-06-04 19:23:40 +0200

    Staff mode begin

--- src/ml/minelounge/core/StaffCommand.java
+++ src/ml/minelounge/core/StaffCommand.java
@@ -1,21 +1,44 @@
 package ml.minelounge.core;
 
+import java.io.File;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+
+import xyz.derkades.utils.InvUtils;
+import xyz.derkades.utils.ItemBuilder;
 
 public class StaffCommand implements CommandExecutor {
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
 		Player player = (Player) sender;
-		player.setAllowFlight(true);
-		player.setFlying(true);
-		player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, Integer.MAX_VALUE, 255, false, false));
-		player.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, Integer.MAX_VALUE, 255, false, false));
-		return true;
+		File file = new File(Main.plugin.getDataFolder() + "/staffinv", player.getUniqueId() + ".yml");
+		
+		if(file.exists()){
+			player.setAllowFlight(false);
+			player.setFlying(false);
+			player.removePotionEffect(PotionEffectType.DAMAGE_RESISTANCE);
+			player.removePotionEffect(PotionEffectType.INVISIBILITY);
+			InvUtils.restoreInventory(file, player.getInventory());
+			return true;
+		} else {
+			player.setAllowFlight(true);
+			player.setFlying(true);
+			player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, Integer.MAX_VALUE, 255, false, false));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, Integer.MAX_VALUE, 255, false, false));
+			InvUtils.saveIntentory(file, player.getInventory());;
+			InvUtils.clearInventory(player.getInventory());
+			ItemStack item = new ItemBuilder(Material.COMPASS).setName(ChatColor.AQUA + "Player Teleporter").setLore(ChatColor.GRAY + "Teleport to players around the server").create();
+			player.getInventory().addItem(item);
+			return true;
+		}
 	}
 }
