42c6e6f88cd977b14c3c3ffee0a6c074fb5ab8b8
RobinMC <robinslot@hotmail.nl>
2017-06-04 17:14:09 +0200

    @Override

--- src/ml/minelounge/core/Main.java
+++ src/ml/minelounge/core/Main.java
@@ -10,6 +10,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
 	
+	@Override	
 	public void onEnable(){
 		getServer().getPluginManager().registerEvents(this, this);
 	}
