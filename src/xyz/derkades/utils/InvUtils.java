541b4ea5562e8650f4139a963f32230fcd9f5d3a
RobinMC <robinslot@hotmail.nl>
2017-06-04 19:04:20 +0200

    Stuff

--- src/xyz/derkades/utils/InvUtils.java
+++ src/xyz/derkades/utils/InvUtils.java
@@ -4,29 +4,28 @@ import java.io.File;
 import java.io.IOException;
 
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 public class InvUtils {
 	
-	public static void saveIntentory(String path, Player player){
+	public static void saveIntentory(File file, PlayerInventory inventory){
 		YamlConfiguration config = new YamlConfiguration();
-		config.set("inventory.armor", player.getInventory().getArmorContents());
-		config.set("inventory.content", player.getInventory().getContents());
+		config.set("inventory.armor", inventory.getArmorContents());
+		config.set("inventory.content", inventory.getContents());
 		try {
-			config.save(new File(path, player.getName() + ".yml"));
+			config.save(file);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
-    public static void restoreInventory(String path, Player player){
-    	File file = new File(path, player.getName()+".yml");
+    public static void restoreInventory(File file, PlayerInventory inventory){
         YamlConfiguration cconfig = YamlConfiguration.loadConfiguration(file);
         ItemStack[] content = cconfig.getList("inventory.armor").toArray(new ItemStack[]{});
-        player.getInventory().setArmorContents(content);
+        inventory.setArmorContents(content);
         content = cconfig.getList("inventory.content").toArray(new ItemStack[]{});
-        player.getInventory().setContents(content);
+        inventory.setContents(content);
         file.delete();
     }
 
